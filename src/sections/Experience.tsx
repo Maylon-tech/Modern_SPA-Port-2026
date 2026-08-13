d38cc3b77c69f0cd4ec1c52552eb67f4a1f72052
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-08-13 12:06:41 +0900

    start implement experience section.

---
+++ src/sections/Experience.tsx
@@ -0,0 +1,22 @@
+
+
+const experience = [
+    {
+        role: "Fullstack Enginner",
+        company: "Independent Projects",
+        perieod: "2025 - Present",
+        description: "Designing and building production-ready web applications with a focus on scalabilitty, performance and clean architecture. Implemented authenctication systems, dashboards and API Integrations.",
+        technologies: ["Next.js", "Typescript", "Drizzle ORM", "PostgreSQL", "Zod"]
+    }
+]
+
+
+const Experience = () => {
+  return (
+    <div>
+      Experience
+    </div>
+  )
+}
+
+export default Experience
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,3 +1,4 @@
+import ProjectCard from "@/components/project/ProjectCard"
 import SectionHeader from "@/components/ui/SectionHeader"
 
 
@@ -5,7 +6,7 @@ const projects = [
     {
         title: "Fullstack SaaS Dashboard",
         description: "A modern analytics dashboard built with Next.js, Prisma and PostGreSQL featuring authentication, charts, and real-time updates.",
-        image: "/images/project1.png/",
+        image: "/images/project1.png",
         tags: ["Next.js", "Typescript", "Prisma"],
         liveURL: "#",
         githubURL: "#"
@@ -13,7 +14,7 @@ const projects = [
     {
         title: "AI Content Generator",
         description: "Generate blog posts and marketing content using AI. Built with OpenAI API, Next.js, and a clean UI system.",
-        image: "/images/project1.png/",
+        image: "/images/project1.png",
         tags: ["Next.js", "OpenAI", "TailwindCSS"],
         liveURL: "#",
         githubURL: "#"
@@ -21,7 +22,7 @@ const projects = [
     {
         title: "Portfolio Website",
         description: "A personal portfolio showcasing projects, skills, and contact features with smooth animations and modern design.",
-        image: "/images/project1.png/",
+        image: "/images/project1.png",
         tags: ["Next.js", "Framer Motion"],
         liveURL: "#",
         githubURL: "#"
@@ -29,7 +30,7 @@ const projects = [
     {
         title: "Real-time Chat Application",
         description: "A Fullstack real-time chat app with authentication, private messaging, and live updates using WebSockets and a scalable backend architecture.",
-        image: "/images/project1.png/",
+        image: "/images/project1.png",
         tags: ["Next.js", "Sock.io", "MongoDB"],
         liveURL: "#",
         githubURL: "#"
@@ -41,15 +42,28 @@ const projects = [
 const Projects = () => {
   return (
     <section id="projects" className="py-24 relative">
-          <div className="absolute top-1/4 left-1/3 -translate-x-1/2 w-80 h-80 rounded-full blur-3xl bg-primary/10" />
-          <SectionHeader
-            title="Some of my recent"
-            highlight="work"
-            badge="Projects"
-            description="A selection of projects showcasing my ability to design, build, and scale modern fullstack applications."
-          />
+        <div className="absolute top-1/4 left-1/3 -translate-x-1/2 w-80 h-80 rounded-full blur-3xl bg-primary/10" />
+        
+        <div className="w-[90%] max-w-6xl mx-auto space-y-12">  
+            <SectionHeader
+                title="Some of my recent"
+                highlight="work"
+                badge="Projects"
+                description="A selection of projects showcasing my ability to design, build, and scale modern fullstack applications."
+            />
+            
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-10">
+                {
+                    projects.map((project, index) => (
+                        <ProjectCard key={index} {...project} />
+                    ))
+                }
+            </div>
+        </div>
     </section>
   )}
 
   
 export default Projects
+ 
